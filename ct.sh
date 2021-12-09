9a42c021aac8cf0b4696ad48a9385ce6f7e299e1
Raoul Strackx <raoul.strackx@fortanix.com>
2021-12-09 12:06:47 +0100

    Setting up CI

--- ct.sh
+++ ct.sh
@@ -1,7 +1,23 @@
 #!/bin/bash -ex
+repo_root=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
+cd ${repo_root}
+
+source ${repo_root}/ct_env.sh
 
 cargo test
 
 pushd tokio
 cargo test --target x86_64-fortanix-unknown-sgx --features "full-sgx"
+popd
+
+pushd tokio-macros
+cargo test --target x86_64-fortanix-unknown-sgx
+popd
+
+pushd tokio-stream
+cargo test --target x86_64-fortanix-unknown-sgx
+popd
+
+pushd stress-test
+cargo test --target x86_64-fortanix-unknown-sgx
 popd
